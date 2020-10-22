291d74c403854628729caf3f847889740be12c0c
LaiqueTauseef <laiqtauseef@gmail.com>
2020-10-22 15:10:54 +0500

    added build for gh-pages

---
+++ frontend/build/precache-manifest.053f8211e2af2c0b4a6ce4dc514980ab.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5fce93778a7eb3a031631bb847c029c",
+    "url": "/Webpage-Assignment2/index.html"
+  },
+  {
+    "revision": "37dc8fdf1c2a627ac83f",
+    "url": "/Webpage-Assignment2/static/css/2.6414bb7b.chunk.css"
+  },
+  {
+    "revision": "69229c521cf1603a4287",
+    "url": "/Webpage-Assignment2/static/css/main.1692a51e.chunk.css"
+  },
+  {
+    "revision": "37dc8fdf1c2a627ac83f",
+    "url": "/Webpage-Assignment2/static/js/2.1e5294d2.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Webpage-Assignment2/static/js/2.1e5294d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69229c521cf1603a4287",
+    "url": "/Webpage-Assignment2/static/js/main.5cf05ac2.chunk.js"
+  },
+  {
+    "revision": "301afe3bb928be062546",
+    "url": "/Webpage-Assignment2/static/js/runtime-main.f8b89335.js"
+  },
+  {
+    "revision": "350ec2cc1af5b433f023a2a767d355e5",
+    "url": "/Webpage-Assignment2/static/media/agridata-512x512.350ec2cc.jpg"
+  },
+  {
+    "revision": "ed64f35528db12cfca3285278fc64044",
+    "url": "/Webpage-Assignment2/static/media/ankor-wat-siem-reap-cambodia.ed64f355.jpg"
+  },
+  {
+    "revision": "c2e0a48f33dc90cf8ffad8d328041611",
+    "url": "/Webpage-Assignment2/static/media/baalbak-lebanon.c2e0a48f.jpg"
+  },
+  {
+    "revision": "61e0d8cc2a4d222e19ab3c39130c1e31",
+    "url": "/Webpage-Assignment2/static/media/boeing-512x512.61e0d8cc.jpg"
+  },
+  {
+    "revision": "ceebfc8a45573fceec77256dba53fac9",
+    "url": "/Webpage-Assignment2/static/media/conduce-512x512.ceebfc8a.jpg"
+  },
+  {
+    "revision": "2ccee179dbfe767b3934ac4e764dc5f7",
+    "url": "/Webpage-Assignment2/static/media/logo-2000x2000-white.2ccee179.png"
+  },
+  {
+    "revision": "c89fcc1a9d3be8dd1da7f9be0a1afb96",
+    "url": "/Webpage-Assignment2/static/media/logo.c89fcc1a.png"
+  },
+  {
+    "revision": "1099fc4739267599f9235df74b265106",
+    "url": "/Webpage-Assignment2/static/media/microsoft.1099fc47.jpg"
+  }
+]);
